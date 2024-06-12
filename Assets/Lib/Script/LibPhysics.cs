543b632e8047d6741811cbe8cee94a6daa554c69
matsuokasvga22 <145432161+matsuokasvga22@users.noreply.github.com>
2024-06-12 17:02:38 +0900

    3D用ができたけど、Debug作業はしてないよ！！

--- Assets/Lib/Script/LibPhysics.cs
+++ Assets/Lib/Script/LibPhysics.cs
@@ -32,6 +32,21 @@ public class LibPhysics
 
 
     /// <summary>
+    /// Physics2D.Raycast (引数をRayに変更(自作))
+    /// </summary>
+    /// <param name="ray"> レイ </param>
+    /// <param name="distance"> 距離 </param>
+    /// <param name="layerMask"> 特定のレイヤーのコライダーのみを判別 (1の場合はDefaultのみ) </param>
+    /// <param name="duration"> 消えるまでの時間(秒単位) (0の場合は1フレームのみ表示) </param>
+    /// <returns></returns>
+    static public RaycastHit2D Raycast2D(Ray ray, float distance, int layerMask = 1, float duration = 0.0f)
+    {
+        return Raycast2D(ray.origin, ray.direction, distance, layerMask, duration);
+    }
+
+
+
+    /// <summary>
     /// Physics2D.Linecast
     /// </summary>
     /// <param name="start"> 開始地点 </param>
@@ -44,6 +59,72 @@ public class LibPhysics
         RaycastHit2D answer = new RaycastHit2D();
 
         answer = Physics2D.Linecast(start, end, layerMask);
+
+#if UNITY_EDITOR
+        Debug.DrawLine(start, end, Color.red, duration);
+#endif
+
+        return answer;
+    }
+
+
+
+    /// <summary>
+    /// Physics.Raycast
+    /// </summary>
+    /// <param name="origin"> 原点 </param>
+    /// <param name="direction"> 方向 </param>
+    /// <param name="distance"> 距離 </param>
+    /// <param name="layerMask"> 特定のレイヤーのコライダーのみを判別 (1の場合はDefaultのみ) </param>
+    /// <param name="duration"> 消えるまでの時間(秒単位) (0の場合は1フレームのみ表示) </param>
+    /// <returns></returns>
+    static public RaycastHit Raycast(Vector3 origin, Vector3 direction, float distance, int layerMask = 1, float duration = 0.0f)
+    {
+        RaycastHit answer = new RaycastHit();
+
+        Physics.Raycast(origin, direction, out RaycastHit hitInfo, distance, layerMask);
+
+        answer = hitInfo;
+
+#if UNITY_EDITOR
+        Debug.DrawRay(origin, direction.normalized * distance, Color.red, duration);
+#endif
+
+        return answer;
+    }
+
+
+
+    /// <summary>
+    /// Physics.Raycast (引数をRayに変更)
+    /// </summary>
+    /// <param name="ray"> レイ </param>
+    /// <param name="distance"> 距離 </param>
+    /// <param name="layerMask"> 特定のレイヤーのコライダーのみを判別 (1の場合はDefaultのみ) </param>
+    /// <param name="duration"> 消えるまでの時間(秒単位) (0の場合は1フレームのみ表示) </param>
+    /// <returns></returns>
+    static public RaycastHit Raycast(Ray ray, float distance, int layerMask = 1, float duration = 0.0f)
+    {
+        return Raycast(ray.origin, ray.direction, distance, layerMask, duration);
+    }
+
+
+
+    /// <summary>
+    /// Physics.Linecast
+    /// </summary>
+    /// <param name="start"> 開始地点 </param>
+    /// <param name="end"> 終了地点 </param>
+    /// <param name="layerMask"> 特定のレイヤーのコライダーのみを判別 (1の場合はDefaultのみ) </param>
+    /// <param name="duration"> 消えるまでの時間(秒単位) (0の場合は1フレームのみ表示) </param>
+    /// <returns></returns>
+    static public RaycastHit Linecast(Vector3 start, Vector3 end, int layerMask = 1, float duration = 0.0f)
+    {
+        RaycastHit answer = new RaycastHit();
+
+        Physics.Linecast(start, end, out RaycastHit hitInfo, layerMask);
+
+        answer = hitInfo;
 
 #if UNITY_EDITOR
         Debug.DrawLine(start, end, Color.red, duration);

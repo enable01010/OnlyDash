e0b42503dda8ccd459ce770798eb4226e1caa004
enable01010 <132036160+enable01010@users.noreply.github.com>
2024-06-13 08:00:23 +0900

    自前のPlayerのスクリプトにJumpの処理を移管

--- Assets/0_Taniyama/Player/Script/Player.cs
+++ Assets/0_Taniyama/Player/Script/Player.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Animator))]
-public class Player : Singleton<Player>
+public class Player : SingletonActionListener<Player>
 {
     private Animator _animator;
     private PlayerInput _playerInput;
     private StarterAssets.StarterAssetsInputs _input;
     private GameObject _mainCamera;
+
+    #region 着地判定用の変数
 
     [Header("着地判定")]
     [Tooltip("地面に接しているが")]
@@ -24,6 +26,25 @@ public class Player : Singleton<Player>
     [Tooltip("地面のレイヤー")]
     [SerializeField] private LayerMask GroundLayers;
 
+    #endregion
+
+    #region ActionListenerの用変数
+
+    private Vector2 move = Vector2.zero;
+    private bool isJump = false;
+    private bool isSlide = false;
+    private bool isSlow = false;
+
+    #endregion
+
+    #region ジャンプ用の変数
+
+    [SerializeField] private float JumpHeight = 1.2f;
+    [SerializeField] private float Gravity = -15.0f;
+    private float _jumpTimeoutDelta;
+    private float _verticalVelocity;
+
+    #endregion
     //アニメーターの設定用
     private int _animIDSpeed;
     private int _animIDGrounded;
@@ -44,10 +65,10 @@ public class Player : Singleton<Player>
         
     }
 
-    public void Jump()
-    {
+    //public void Jump()
+    //{
 
-    }
+    //}
 
     public void Fall()
     {
@@ -91,4 +112,52 @@ public class Player : Singleton<Player>
 
         _animator.SetBool(_animIDGrounded, isGrounded); 
     }
+
+    #region ActionListenerのコールバック関数
+
+    public override void OnPlayerMove(InputAction.CallbackContext context)
+    {
+        base.OnPlayerMove(context);
+
+        move = context.ReadValue<Vector2>();
+    }
+
+    public override void OnJump(InputAction.CallbackContext context)
+    {
+        base.OnJump(context);
+
+        isJump = context.ReadValue<bool>();
+
+        if(isGrounded == true)
+        {
+            // Jump
+            if (_input.jump && _jumpTimeoutDelta <= 0.0f)
+            {
+                // the square root of H * -2 * G = how much velocity needed to reach desired height
+                _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+
+                // update animator if using character
+                
+                _animator.SetBool(_animIDJump, true);
+                
+            }
+        }
+    }
+
+    public override void OnSlow(InputAction.CallbackContext context)
+    {
+        base.OnSlow(context);
+
+        isSlow = context.ReadValue<bool>();
+    }
+
+    public override void OnSlide(InputAction.CallbackContext context)
+    {
+        base.OnSlide(context);
+
+        isSlide = context.ReadValue<bool>();
+    }
+
+    #endregion
+
 }

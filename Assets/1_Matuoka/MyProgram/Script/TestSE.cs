0517fceeffd617bf17bb608718d470763c9df4c2
matsuokasvga22 <145432161+matsuokasvga22@users.noreply.github.com>
2024-06-19 03:20:08 +0900

    LibSoundを書き直したよ！！

---
+++ Assets/1_Matuoka/MyProgram/Script/TestSE.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSE : MonoBehaviour
+{
+
+    public BGMName bGMName = BGMName.BGM1;
+    public SoundFxName soundFxName1 = SoundFxName.‹Õ‚ÌŠŠ‘t;
+    public SoundFxName soundFxName2 = SoundFxName.‹Õ‚ÌŠŠ‘t;
+    public SoundFxName soundFxName3 = SoundFxName.‹Õ‚ÌŠŠ‘t;
+    public SoundFxName soundFxName4 = SoundFxName.‹Õ‚ÌŠŠ‘t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void aaa1()
+    {
+        LibSound.StopAll();
+    }
+
+
+    public void aaa2()
+    {
+        LibSound.PlayBGM(bGMName);
+    }
+
+    public void bbb1()
+    {
+        LibSound.PlaySoloSE(soundFxName1);
+    }
+
+    public void bbb2()
+    {
+        LibSound.PlaySoloSE(soundFxName2);
+    }
+
+    public void bbb3()
+    {
+        LibSound.PlaySoloSE(soundFxName3);
+    }
+
+    public void bbb4()
+    {
+        LibSound.PlaySoloSE(soundFxName4);
+    }
+
+    public void ccc1()
+    {
+        LibSound.PlayTrollSE(soundFxName1);
+    }
+
+    public void ccc2()
+    {
+        LibSound.PlayTrollSE(soundFxName2);
+    }
+
+    public void ccc3()
+    {
+        LibSound.PlayTrollSE(soundFxName3);
+    }
+
+    public void ccc4()
+    {
+        LibSound.PlayTrollSE(soundFxName4);
+    }
+
+
+}

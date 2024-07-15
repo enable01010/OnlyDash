020df4b6a03b00fb7cba3d27aec5edc5ba138fa2
enable01010 <t.rikuto.vga22@gmail.com>
2024-07-15 15:37:03 +0900

    インターフェースシリアライズ用のスクリプト追加

---
+++ Assets/0_Taniyama/Others/InterfaceSerializedTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceSerializedTest : MonoBehaviour
+{
+    [SerializeReference, SubclassSelector] I_Test test;
+
+
+    
+    public interface I_Test
+    {
+
+    }
+
+    [System.Serializable]
+    public class I_Test_1 : I_Test
+    {
+
+    }
+
+    [System.Serializable]
+    public class I_Test_2 : I_Test
+    {
+
+    }
+}

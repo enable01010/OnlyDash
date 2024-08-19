78b66b739551159138150b0fdd3878756926855a
enable01010 <t.rikuto.vga22@gmail.com>
2024-08-20 00:05:51 +0900

    スティックちょっと倒すとゆっくり足を動かすバグの修正

--- Assets/1_Matuoka/MyProgram/Script/I_Move.cs
+++ Assets/1_Matuoka/MyProgram/Script/I_Move.cs
@@ -18,59 +18,11 @@ public partial class Player : SingletonActionListener<Player>
     {
         public virtual void Move()
         {
-            Player instance = Player.instance;
-            // 走る最大速度
-            float targetSpeed = instance.SPRINT_SPEED;
-
-            // コントローラー入力
-            if (instance.playerMove == Vector2.zero) targetSpeed = 0.0f;
-
-            // 現在の速度
-            float currentHorizontalSpeed = new Vector3(instance._controller.velocity.x, 0.0f, instance._controller.velocity.z).magnitude;
-
-            // ？？？
-            const float SPEED_OFFSET = 0.1f;
-
-            // コントローラー入力のMagnitude
-            float inputMagnitude = instance.playerMove.magnitude;
-            //float inputMagnitude = 1;
-
-
-
-            // 速度計算
-
-            if (currentHorizontalSpeed < targetSpeed - SPEED_OFFSET || currentHorizontalSpeed > targetSpeed + SPEED_OFFSET)
-            {
-                // creates curved result rather than a linear one giving a more organic speed change
-                // note T in Lerp is clamped, so we don't need to clamp our speed
-                // 徐々に速度を変える？
-                instance._speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
-                    Time.deltaTime * instance.SPEED_CHANGE_RATE);
-
-                // 速度を小数点以下3桁に丸める
-                // Mathf.Round 最も近い整数を返す(端数が.5の場合は最も近い偶数を返す)
-                instance._speed = Mathf.Round(instance._speed * 1000f) / 1000f;
-            }
-            else
-            {
-                instance._speed = targetSpeed;
-            }
-
-            instance._animationBlend = Mathf.Lerp(instance._animationBlend, targetSpeed, Time.deltaTime * instance.SPEED_CHANGE_RATE);
-            if (instance._animationBlend < 0.01f) instance._animationBlend = 0f;
-
-            // 進行方向
+            instance._speed = instance.playerMove.magnitude * instance.MOVE_SPEED;
+            instance._animationBlend = (instance._speed < 0.1f)?0 : instance._speed;
             Vector3 targetDirection = Quaternion.Euler(0.0f, instance._targetRotation, 0.0f) * Vector3.forward;
-            //Vector3 targetDirection = instance.transform.forward;
-
             instance._controller.Move(targetDirection.normalized * (instance._speed * Time.deltaTime) + new Vector3(0.0f, instance._verticalVelocity, 0.0f) * Time.deltaTime);
-
-
-            // アニメーター関連
             instance._animator.SetFloat(instance._animIDSpeed, instance._animationBlend);
-            instance._animator.SetFloat(instance._animIDMotionSpeed, inputMagnitude);
-
-
         }
     }
 

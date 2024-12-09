f6bd16823ecfdf4a40870ee5fd6875aad16f08f1
matsuokasvga22 <145432161+matsuokasvga22@users.noreply.github.com>
2024-12-09 19:05:06 +0900

    Buttonが複製されるよ！！

--- Assets/1_Matuoka/MyProgram/Script/DebugTool.cs
+++ Assets/1_Matuoka/MyProgram/Script/DebugTool.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using System.Collections.Generic;
 using static Player;
 /// <summary>
-/// isPause
-/// DebugToolOff
-/// DebugToolOn
 /// SwitchBlock
 /// </summary>
 
@@ -14,7 +12,7 @@ public class DebugTool : MonoBehaviour
 #if UNITY_EDITOR
 
     #region Field
-    private bool isPause = false;
+    private bool isDebugTool = false;
     [SerializeField] private GameObject uiWindow;
 
     private enum PlayerPrefsEnum
@@ -45,10 +43,15 @@ public class DebugTool : MonoBehaviour
 
 
     [SerializeField] private GameObject content;
+    [SerializeField] private InputField inputField;
+    [SerializeField] private Button savePosButton;
     [SerializeField, Range(1, 10)] private int SAVE_LIMIT = 10;
-    private GameObject[] buttonsObj;
-    private Button[] buttons;
-    private Text[] buttonsText;
+    public List<string> posNames = new();
+    public List<Vector3> posVectors = new(); 
+    public List<GameObject> posButtonsObj = new();
+    public List<Button> posButtons = new();
+    public List<Text> posButtonsText = new();
+    public Vector3 playerPos;
 
     #endregion
 
@@ -59,13 +62,15 @@ public class DebugTool : MonoBehaviour
     {
         DataLoad();
         SetUiActions();
+        posButtonsObj.Add(content.transform.GetChild(0).gameObject);
+        SavePosButtonChange();
         SavePosInit();
     }
 
     private void Start()
     {
         DataSet();
-        ReSetColorPlayerMoveButton();
+        ResetColorPlayerMoveButton();
     }
 
     private void Update()
@@ -86,6 +91,7 @@ public class DebugTool : MonoBehaviour
     {
         timeScale = PlayerPrefs.GetFloat(PlayerPrefsEnum.TimeScale.ToString(), 1f);
         playerMove = (PlayerMove)PlayerPrefs.GetInt(PlayerPrefsEnum.PlayerMove.ToString());
+        SavePosLoad();
     }
 
     /// <summary>
@@ -118,6 +124,7 @@ public class DebugTool : MonoBehaviour
     {
         TimeScaleChange();
         PlayerMoveChange();
+        PlayerPosChange();
     }
 
     /// <summary>
@@ -136,31 +143,33 @@ public class DebugTool : MonoBehaviour
     /// </summary>
     private void InputKey()
     {
-        if (isPause == false)
+        if (isDebugTool == false)
         {
-            DebugToolOn();
+            DebugToolOpen();
         }
         else
         {
-            DebugToolOff();
+            DebugToolClose();
         }
 
-        isPause = !isPause;
-        uiWindow.SetActive(isPause);
+        isDebugTool = !isDebugTool;
+        uiWindow.SetActive(isDebugTool);
     }
 
     /// <summary>
     /// DebugToolをONにする処理
     /// </summary>
-    private void DebugToolOn()
+    private void DebugToolOpen()
     {
         Time.timeScale = 0f;
+
+        playerPos = Player.instance.transform.position;
     }
 
     /// <summary>
     /// DebugToolをOFFにする処理
     /// </summary>
-    private void DebugToolOff()
+    private void DebugToolClose()
     {
         DataSave();
         DataSet();
@@ -196,20 +205,25 @@ public class DebugTool : MonoBehaviour
         }
     }
 
+    private void PlayerPosChange()
+    {
+        Player.instance.transform.position = playerPos;
+    }
+
     /// <summary>
     /// 移動方法を切り替えるボタン
     /// </summary>
     private void PlayerMoveButton(PlayerMove playerMove)
     {
         this.playerMove = playerMove;
-        ReSetColorPlayerMoveButton();
+        ResetColorPlayerMoveButton();
     }
 
 
     /// <summary>
     /// プレイヤーの移動方法指定ボタンの色を調整する処理
     /// </summary>
-    private void ReSetColorPlayerMoveButton()
+    private void ResetColorPlayerMoveButton()
     {
         int length = Enum.GetValues(typeof(PlayerMove)).Length;
         for (int i = 0; i < length; i++)
@@ -227,20 +241,67 @@ public class DebugTool : MonoBehaviour
 
     private void SavePosInit()
     {
-        buttonsObj = new GameObject[SAVE_LIMIT];
-        buttons = new Button[SAVE_LIMIT];
-        buttonsText = new Text[SAVE_LIMIT];
+        playerPos = Player.instance.transform.position;
+    }
 
-        buttonsObj[0] = content.transform.GetChild(0).gameObject;
+    private void SavePosLoad()
+    {
         for (int i = 0; i < SAVE_LIMIT; i++)
         {
-            if(i != 0) buttonsObj[i] = Instantiate(buttonsObj[0], content.transform);
+            string @tempString = PlayerPrefs.GetString(PlayerPrefsEnum.SavePosName.ToString() + i, "None");
 
-            buttons[i] = buttonsObj[i].GetComponent<Button>();
-            buttonsText[i] = buttonsObj[i].GetComponentInChildren<Text>();
+            if (@tempString == "None") break;
 
-            buttonsText[i].text = PlayerPrefs.GetString(PlayerPrefsEnum.SavePosName.ToString() + i, "None");
+            posNames.Add(@tempString);
+
+            Vector3 @tempVector3 = Vector3.zero;
+
+            @tempVector3.x = PlayerPrefs.GetFloat(PlayerPrefsEnum.SavePosX.ToString() + i);
+            @tempVector3.y = PlayerPrefs.GetFloat(PlayerPrefsEnum.SavePosY.ToString() + i);
+            @tempVector3.z = PlayerPrefs.GetFloat(PlayerPrefsEnum.SavePosZ.ToString() + i);
+
+            posVectors.Add(tempVector3);
+        }  
+    }
+
+
+    private void SavePosButtonChange()
+    {
+        for (int i = 0; i < posNames.Count; i++)
+        {
+            if(i != 0) posButtonsObj.Add(Instantiate(posButtonsObj[0], content.transform));
+
+            posButtons.Add(posButtonsObj[i].GetComponent<Button>());
+            posButtonsText.Add(posButtonsObj[i].GetComponentInChildren<Text>());
+
+            posButtonsText[i].text = PlayerPrefs.GetString(PlayerPrefsEnum.SavePosName.ToString() + i, "None");
         }
+
+        posButtonsObj[0].SetActive(posNames.Count != 0);
+    }
+
+    public void SavePosSaveButton()
+    {
+        posNames.Add(inputField.text);
+        inputField.text = "";
+        PlayerPrefs.SetString(PlayerPrefsEnum.SavePosName.ToString() + (posNames.Count - 1), posNames[^1]);
+
+        posVectors.Add(Player.instance.transform.position);
+        PlayerPrefs.SetFloat(PlayerPrefsEnum.SavePosX.ToString() + (posVectors.Count - 1), posVectors[^1].x);
+        PlayerPrefs.SetFloat(PlayerPrefsEnum.SavePosY.ToString() + (posVectors.Count - 1), posVectors[^1].y);
+        PlayerPrefs.SetFloat(PlayerPrefsEnum.SavePosZ.ToString() + (posVectors.Count - 1), posVectors[^1].z);
+
+        PlayerPrefs.Save();
+
+
+        posButtonsObj[0].SetActive(true);
+
+        if (posButtonsObj.Count != posNames.Count) posButtonsObj.Add(Instantiate(posButtonsObj[0], content.transform));
+
+        posButtons.Add(posButtonsObj[^1].GetComponent<Button>());
+        posButtonsText.Add(posButtonsObj[^1].GetComponentInChildren<Text>());
+
+        posButtonsText[^1].text = posNames[^1];
     }
 
     #endregion
